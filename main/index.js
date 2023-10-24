1644ac292c4366ffc3a6d4a24b7f580ebdc80c43
Roman <ketzalec@gmail.com>
2023-10-24 13:40:53 +0300

    eeennnd

--- main/index.js
+++ main/index.js
@@ -13,6 +13,6 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(1000, () => {
+app.listen(1100, () => {
   console.log("Running");
 });
