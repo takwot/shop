248a0225796f2aeb649a6f02b475f99672871c33
Roman <ketzalec@gmail.com>
2023-10-24 14:04:15 +0300

    asd

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { Client } = require("pg");
 
 const client = new Client({
   user: "postgres",
-  host: "localhost",
+  host: "database",
   database: "shop",
   password: "qwerty",
   port: "5432",
