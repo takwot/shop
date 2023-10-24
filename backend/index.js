fdec21cdecc9be63842233fecc97dd51371c2f16
Roman <ketzalec@gmail.com>
2023-10-24 14:55:26 +0300

    adasda

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const ItemRouter = require("./router/ItemsRouter");
+const client = require("./db");
 
 const app = express();
 
@@ -9,6 +10,19 @@ app.use(express.json({}));
 app.use(cors());
 
 app.use("/api", ItemRouter);
+app.get("/api/create-list", (req, res) => {
+  client
+    .query(
+      "create table items(ID SERIAL PRIMARY KEY, name varchar(255), description varchar(255),telegramm varchar(255);"
+    )
+    .then(() => {
+      res.json({ message: "Success" }).statusCode(200);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({ message: "Fail" }).statusCode(500);
+    });
+});
 
 app.listen(5000, () => {
   console.log("Server is running");
